8df8dbb0cac8feef48a913a94da45fd3a881d707
asimaarbi <asimfarooq5@gmail.com>
2021-02-05 23:40:35 +0500

    updates

--- automate.sh
+++ automate.sh
@@ -2,35 +2,39 @@
 set -e
 
 while ! ping -c 1 -W 1 8.8.8.8; do
-    echo "Waiting for 1.2.3.4 - network interface might be down..."
-    sleep 1
+  echo "Waiting for 1.2.3.4 - network interface might be down..."
+  sleep 1
 done
 
 if [[ $(id -u) -ne 0 ]]; then
-    echo Must run as root
-    exit 1
+  echo Must run as root
+  exit 1
 fi
 
-#ID=$(/usr/bin/anydesk --get-id)
-#pass=$(openssl rand -hex 4)
-#echo $pass | anydesk --set-password
+ID=$(/usr/bin/anydesk --get-id)
+pass=$(openssl rand -hex 4)
+echo $pass | anydesk --set-password
 
-#email=thomasseher@gmx.de
-
-#phone=+491788773695
-#email=$(sqlite3 /home/pi/recipient.db "SELECT email FROM recipient")
-email=(asimfarooq5@gmail.com)
-
-for value in $email
-do
+#switches=$(python3 switchState2.py)
+switches=3
+for switch in $switches; do
+  emails=$(curl -s http://94.130.187.90:7777/api/recipients/9/2 | jq -r .emails[])
+  phones=$(curl -s http://94.130.187.90:7777/api/recipients/9/2 | jq -r .phones[])
+  echo $emails
+  echo $switch
+  for value in $emails; do
     echo $value
-    curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd   --mail-from 'grandmahertha9@gmail.com'   --mail-rcpt $value   --user 'grandmahertha9@gmail.com:thomaspi'   -T <(echo -e "From:
-                   grandmahertha9@gmail.com\nTo: bernhardweb@web.de\nSubject:Time to Meet\n\nAnydesk Id : $ID\nPassword  : $pass\nJitsi Link :  
-                   http://codebase.pk:7300/connect/telemarie/jitsi\nAdmin panel link : http://codebase.pk:7778/admin")
+    curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd --mail-from 'grandmahertha9@gmail.com' --mail-rcpt $value --user 'grandmahertha9@gmail.com:thomaspi' -T <(echo -e "From: grandmahertha9@gmail.com\nTo: thomasseher@gmx.de\nSubject:Telemarie 2 ist eingeschaltet\n\nAnydesk Id : $ID\nPassword  : $pass\nJitsi Link :  http://94.130.187.90:7300/connect/telemarie/jitsi/tm/2\n\nPower_off tm2 : http://94.130.187.90:7300/tm/2/poweroff\n\n Reboot tm2 : http://94.130.187.90:7300/tm/2/reboot\r\n")
+  done
+  sleep 20
+  for value in $phones; do
+    stty -F /dev/ttyUSB3 speed 9600 -brkint -icrnl ixoff -imaxbel -opost -onlcr -isig -icanon -echo -echoe
+    tail -f /dev/ttyUSB3 &
+    phone="+"$value
+    echo $phone
+    echo -e "AT+CMGF=1\r\n" >/dev/ttyUSB3
+    echo -e "AT+CMGS=\"$phone\"\r\n" >/dev/ttyUSB3
+    echo -e "Telemarie2 eingeschaltet\nAnydeskId:$ID\nPassword:$pass\nMeet: 94.130.187.90:7300/connect/telemarie/jitsi/tm/2\nPoweroff: 94.130.187.90:7300/tm/2/poweroff\032\r\n" >/dev/ttyUSB3
+    sleep 10
+  done
 done
-
-#stty -F /dev/ttyUSB3 speed 9600 -brkint -icrnl ixoff -imaxbel -opost -onlcr -isig -icanon -echo -echoe
-#tail -f /dev/ttyUSB3 &
-#echo -e "AT+CMGF=1\r\n" > /dev/ttyUSB3
-#echo -e "AT+CMGS=\"$phone\"\r\n" > /dev/ttyUSB3
-#echo -e "Hey, Time to meet with Grandma\n Id : $ID\n pass: $pass\nJitsi Link: http://codebase.pk:7300/connect/telemarie/jitsi\032\r\n" > /dev/ttyUSB3
--- provider.py
+++ provider.py
@@ -12,59 +12,23 @@ from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from autobahn.wamp import register
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipient.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-
-class Recipient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    uid = db.Column(db.Integer)
-    email = db.Column(db.String(50), nullable=True)
-    phone = db.Column(db.String(255), nullable=True)
-
-
-users = Recipient.query.all()
-for user in users:
-    print(user.email, user.phone)
 
 urls = []
+
+
 class OpenerSession(ApplicationSession):
     @register('url.open')
     def _url_open(self, url):
         if url in urls:
             print('link already opened')
         else:
-            if url == 'https://meet.jit.si/meet-grandma-hertau#config.prejoinPageEnabled=false':
+            if url == 'https://meet.jit.si/tm/1#config.prejoinPageEnabled=false':
                 urls.append(url)
             p = multiprocessing.Process(target=self.open, args=(url,))
             p.start()
 
     def open(self, url):
-        subprocess.check_call(shlex.split(f"chromium-browser {url}"))
-
-    @register(None)
-    def recipient(self, uid, email, phone, status):
-        if status == 'delete':
-            recipient = Recipient.query.filter((Recipient.email == email)).first()
-            print(recipient.email)
-            db.session.delete(recipient)
-            db.session.commit()
-            print("delete")
-        elif status == 'update':
-            recipient = Recipient.query.filter((Recipient.uid == uid)).first()
-            recipient.email = email
-            recipient.phone = phone
-            db.session.commit()
-            print("update")
-        else:
-            recipient = Recipient()
-            recipient.uid = uid
-            recipient.email = email
-            recipient.phone = phone
-            db.session.add(recipient)
-            db.session.commit()
-            print("created")
+        subprocess.check_call(shlex.split(f"chromium-browser --start-fullscreen {url}"))
 
     @register(None)
     def message(self, message):
@@ -78,12 +42,19 @@ class OpenerSession(ApplicationSession):
         msg_box.show()
         msg_box.exec_()
 
+    @register(None)
+    def power(self, command):
+        if command == 'poweroff':
+            subprocess.check_call(shlex.split(f"{command}"))
+        elif command == 'reboot':
+            subprocess.check_call(shlex.split(f"{command}"))
+
     async def onJoin(self, _details):
-        regs = await self.register(self, prefix="org.deskconn.")
+        regs = await self.register(self, prefix="tm.1.")
         for reg in regs:
             self.log.info("Registered procedure {procedure}", procedure=reg.procedure)
 
-if __name__ == '__main__':
-    runner = ApplicationRunner("ws://codebase.pk:9002/ws", realm="realm1")
-    runner.run(OpenerSession)
 
+if __name__ == '__main__':
+    runner = ApplicationRunner("ws://94.130.187.90:8080/ws", realm="realm1")
+    runner.run(OpenerSession)
